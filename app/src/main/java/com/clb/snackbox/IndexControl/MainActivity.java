b2b4cf4e41b1d5e20312caae7d091ebcdbf20a3b
dddddhhhhh <1658239796@qq.com>
2018-01-29 20:48:08 +0800

    Signed-off-by: dddddhhhhh <1658239796@qq.com>

---
+++ app/src/main/java/com/clb/snackbox/IndexControl/MainActivity.java
@@ -0,0 +1,18 @@
+package com.clb.snackbox.IndexControl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.clb.snackbox.R;
+
+/**
+ * Created by 幻 on 2018/1/29.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- app/src/main/java/com/clb/snackbox/launch/LaunchUI.java
+++ app/src/main/java/com/clb/snackbox/launch/LaunchUI.java
@@ -1,18 +1,35 @@
 package com.clb.snackbox.launch;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.clb.snackbox.IndexControl.MainActivity;
 import com.clb.snackbox.R;
+import com.uuzuche.lib_zxing.activity.CaptureActivity;
+import com.uuzuche.lib_zxing.activity.CodeUtils;
+import com.uuzuche.lib_zxing.activity.ZXingLibrary;
+
+import static android.R.attr.data;
 
 /**
  * 引导界面，可以显示打开app的界面。。然后正式进入app
  */
 public class LaunchUI extends Activity {
-
+    private int REQUEST_CODE;
+    private int requestCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ZXingLibrary.initDisplayOpinion(this);
+        Intent intent = new Intent( LaunchUI.this, CaptureActivity.class);
+        startActivityForResult(intent, REQUEST_CODE);
+
+        if (requestCode == REQUEST_CODE) {
+            //处理扫描结果（在界面上显示）
+            setContentView(R.layout.activity_main);
+
+        }
     }
 }
